3627370c82d8d00bea61b0d949ea93267e14439e
HasinduOnline <40927446+hrtechplus@users.noreply.github.com>
2024-08-01 17:47:58 +0530

    chore: Update sci-fi page component to display specific book ID

--- nexfirst/src/app/books/sci-fi/[id]/page.jsx
+++ nexfirst/src/app/books/sci-fi/[id]/page.jsx
@@ -1,5 +1,5 @@
 import React from "react";
 
-export default function page() {
-  return <div>sci-fi book 1 is here</div>;
+export default function page({ params }) {
+  return <div>sci-fi book {params.id} is here</div>;
 }
